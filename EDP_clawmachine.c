6062920b5188dca7827ab5829cdbdaf68b07d13a
Natuno Kiu <131436290+NatunoKiu@users.noreply.github.com>
2025-08-08 22:12:17 +0900

    サーボモーターSG90の回転をスイッチで制御するプログラムの作成。 (#1)
    * サーボモーターSG90の初期化処理の追加。
    
    * パルス幅を計算する関数set_pwm_pulseを使ってサーボモーターへPWM信号を送る処理を追加。
    
    * コメント修正。
    
    * モーターM1, M2の正転逆転処理を追加。

--- EDP_clawmachine.c
+++ EDP_clawmachine.c
@@ -1,13 +1,22 @@
 #include <stdio.h>
 
+#include "hardware/pwm.h"
 #include "pico/stdlib.h"
+
+static uint16_t set_pwm_pulse(uint16_t period_cycle, float cycletime,
+                              float pulse_width) {
+    float pwm_count_deg = (float)period_cycle / cycletime * pulse_width;
+    return (uint16_t)pwm_count_deg;
+};
 
 int main() {
     stdio_init_all();
-    const uint8_t M1_IN1 = 0;
+    const uint8_t M1_IN1 = 25;
     const uint8_t M1_IN2 = 1;
     const uint8_t S1 = 2;
     const uint8_t S2 = 3;
+
+    const uint8_t SURVO = 14;
 
     // モーター初期化
     gpio_init(M1_IN1);
@@ -16,6 +25,25 @@ int main() {
     gpio_set_dir(M1_IN2, GPIO_OUT);
     gpio_put(M1_IN1, 0);
     gpio_put(M1_IN2, 0);
+
+    // サーボモータ初期化
+    static uint16_t PERIOD_CYCLE = 25000;
+    static float CYCLETIME = 20.0F;
+    static float PULSE_WIDTH_DEG_M60 = 0.82F;
+    static float PULSE_WIDTH_DEG_P60 = 2.10F;
+    uint16_t pulse1 =
+        set_pwm_pulse(PERIOD_CYCLE, CYCLETIME, PULSE_WIDTH_DEG_M60);
+
+    uint16_t pulse2 =
+        set_pwm_pulse(PERIOD_CYCLE, CYCLETIME, PULSE_WIDTH_DEG_P60);
+
+    gpio_set_function(SURVO, GPIO_FUNC_PWM);
+    uint slice_num = pwm_gpio_to_slice_num(SURVO);
+    // 分解能を指定
+    pwm_set_wrap(slice_num, 25000 - 1);
+    // 分周比を指定
+    pwm_set_clkdiv(slice_num, 100.0f);
+    pwm_set_enabled(slice_num, true);
 
     // スイッチ初期化
     gpio_init(S1);
@@ -27,15 +55,15 @@ int main() {
 
     while (true) {
         if (!gpio_get(S2)) {
-            while (true) {
-                if (!gpio_get(S1)) {
-                    gpio_put(M1_IN1, 1);
-                    gpio_put(M1_IN2, 0);
-                } else {
-                    gpio_put(M1_IN1, 0);
-                    gpio_put(M1_IN2, 1);
-                }
-            }
+            gpio_put(M1_IN1, 1);
+            gpio_put(M1_IN2, 0);
+            pwm_set_gpio_level(SURVO, pulse1);
+            sleep_ms(500);
+        } else {
+            gpio_put(M1_IN1, 0);
+            gpio_put(M1_IN2, 1);
+            pwm_set_gpio_level(SURVO, pulse2);
+            sleep_ms(500);
         }
     }
     return 0;
